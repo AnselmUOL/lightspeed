c9d17885730fe56001a1433a88d7bb5b75512079
AnselmUOL <anselm.lohmann@uni-oldenburg.de>
2026-02-13 14:41:40 +0100

    Added complex number functionality to flops_col_sum.m

--- flops_col_sum.m
+++ flops_col_sum.m
@@ -1,4 +1,4 @@
-function f = flops_col_sum(n,m)
+function f = flops_col_sum(n,m,mode)
 % FLOPS_COL_SUM         Flops for column sums.
 % FLOPS_COL_SUM(a) returns the number of flops for col_sum(a).
 % FLOPS_COL_SUM(n,m) returns the number of flops for col_sum(ones(n,m)).
@@ -7,4 +7,15 @@ if nargin == 1
   m = cols(n);
   n = rows(n);
 end
-f = (n-1)*m;
+if nargin < 3
+  mode = "real";
+end
+if mode == "real"
+  f = (n-1)*m;
+elseif mode == "complex"
+  f = 2*(n-1)*m;
+else
+  error('Invalid mode specified. Use "real" or "complex".');
+end
+
+end
